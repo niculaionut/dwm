4d9aab8eb7ad71ca2f0fdb0fccafd6ac312c6d0a
Nicula Ionut <niculaionut@tutanota.com>
2020-12-01 17:43:08 +0200

    patch:attachsideandbelow

--- config.h
+++ config.h
@@ -55,7 +55,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray5, "-nf", col_gray6, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray5, "-nf", col_gray6, "-sb", col_cyan, "-sf", col_gray5, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 
